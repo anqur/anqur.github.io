f90a8a2c986cde47793220d383f7c78ff496a508
anqurvanillapy <luxingyu@kingsoft.com>
2017-11-06 09:29:04 +0800

    added Fetching spinner

--- index.js
+++ index.js
@@ -2,24 +2,74 @@ import qs from 'querystring'
 import marked from 'marked'
 import hljs from 'highlight.js/lib/highlight'
 
+import langset from './langset'
+
 import './style.css'
 import 'highlight.js/styles/default.css'
 
+const [
+  $lastModified,
+  $footerTitle,
+  $scrollToTop,
+  $spinner,
+  $loader
+] = [
+  'last-modified',
+  'footer-title',
+  'scroll-to-top',
+  'spinner',
+  'loader'
+].map(id => document.getElementById(id))
+
+class Spinner {
+  constructor () {
+    this.arr = ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷']
+    this.idx = 0
+    this.timer = setInterval(this._update, 75)
+    this.$spinner = document.getElementById('spinner')
+  }
+
+  spin = () => {
+    $spinner.textContent = this.arr[this.idx]
+  }
+  destroy = () => {
+    clearInterval(this.timer)
+    $loader.remove()
+  }
+  _update = () => {
+    let idx = this.idx
+    idx = idx === this.arr.length - 1 ? 0 : idx + 1
+    this.idx = idx
+    this.spin()
+  }
+}
+
+
+const render = (title, body, mod) => {
+  document.title = title
+  document.querySelector('main').insertAdjacentHTML('beforeend', marked(body))
+  $lastModified.textContent = mod
+  $footerTitle.textContent = title
+}
+
 ;(async () => {
+  // Boot.
+
+  const spinner = new Spinner()
+  spinner.spin()
+
   // Scroll to top.
 
-  document.getElementById('scroll-to-top').onclick = () => {
+  $scrollToTop.onclick = () => {
     window.scrollTo(0, 0)
     return false
   }
 
   // Code highlighting.
 
-  ;[
-    'javascript', 'python', 'bash', 'cpp', 'java', 'go', 'clojure', 'scala'
-  ].forEach(lang => {
-    const module = require(`highlight.js/lib/languages/${lang}`)
-    hljs.registerLanguage(lang, module)
+  langset.forEach(lang => {
+    const hlmodule = require(`highlight.js/lib/languages/${lang}`)
+    hljs.registerLanguage(lang, hlmodule)
   })
 
   marked.setOptions({
@@ -40,12 +90,7 @@ import 'highlight.js/styles/default.css'
 
   const lastModified = res.headers.get('last-modified')
   const body = await res.text()
-  render(title, body, lastModified)
 
-  function render (title, body, mod) {
-    document.title = title
-    document.querySelector('main').insertAdjacentHTML('beforeend', marked(body))
-    document.getElementById('last-modified').textContent = mod
-    document.getElementById('footer-title').textContent = title
-  }
+  spinner.destroy()
+  render(title, body, lastModified)
 })()
---
+++ langset.js
@@ -0,0 +1,12 @@
+const langset = [
+  'javascript',
+  'python',
+  'bash',
+  'cpp',
+  'java',
+  'go',
+  'clojure',
+  'scala'
+]
+
+export default langset
