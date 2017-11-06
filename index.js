c196198fbe1467e18eee92ffd9b120e4cfcf7e9e
anqurvanillapy <luxingyu@kingsoft.com>
2017-11-06 11:52:30 +0800

    updated js: more compact

--- index.js
+++ index.js
@@ -29,18 +29,20 @@ class Spinner {
     $loader.insertAdjacentHTML('beforeend', 'Fetching...')
   }
 
-  spin = () => {
+  _spin = () => {
     $spinner.textContent = this.arr[this.idx]
   }
+
   destroy = () => {
     clearInterval(this.timer)
     $loader.remove()
   }
+
   _update = () => {
     let idx = this.idx
     idx = idx === this.arr.length - 1 ? 0 : idx + 1
     this.idx = idx
-    this.spin()
+    this._spin()
   }
 }
 
@@ -53,44 +55,24 @@ const render = (title, body, mod) => {
 }
 
 ;(async () => {
-  // Boot.
-
   const spinner = new Spinner()
-  spinner.spin()
-
-  // Scroll to top.
-
   $scrollToTop.onclick = () => {
     window.scrollTo(0, 0)
     return false
   }
 
-  // Code highlighting.
-
-  langset.forEach(lang => {
-    const hlmodule = require(`highlight.js/lib/languages/${lang}`)
-    hljs.registerLanguage(lang, hlmodule)
-  })
-
-  marked.setOptions({
-    highlight: code => {
-      return hljs.highlightAuto(code).value
-    }
-  })
-
-  // Routes.
+  langset.forEach(lang => hljs.registerLanguage(lang,
+    require(`highlight.js/lib/languages/${lang}`)))
+  marked.setOptions({ highlight: code => hljs.highlightAuto(code).value })
 
   const title = qs.parse(window.location.search.slice(1)).p || 'contents'
   const res = await window.fetch(`/content/${title}.md`)
-
-  if (!res.ok) {
-    render('Error', '```bash\n$ echo $?\n404 # :(\n```', 'Page Not Found')
-    return
-  }
-
-  const lastModified = res.headers.get('last-modified')
-  const body = await res.text()
-
   spinner.destroy()
-  render(title, body, lastModified)
+  if (res.ok) {
+    const lastModified = res.headers.get('last-modified')
+    const body = await res.text()
+    render(title, body, lastModified)
+  } else {
+    render('Error', '```bash\n$ echo $?\n404 # :(\n```', 'Page Not Found')
+  }
 })()
