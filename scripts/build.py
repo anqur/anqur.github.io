c05d9d488dd199eb474630d737d4bc9261a24565
anqurvanillapy <anqurvanillapy@gmail.com>
2020-03-25 17:55:02 +0800

    [ script ] Parallelism, ayo!

--- scripts/build.py
+++ scripts/build.py
@@ -2,9 +2,11 @@ import argparse
 import subprocess as sp
 from pathlib import Path
 
-def collect(is_force):
-    pending_items = []
+# Why not `asyncio`?  You are thinking peach!  That's not parallelism!
+from multiprocessing import Process, JoinableQueue, Lock, Value
 
+
+def collector(pending_items, lock, total, is_force):
     for post in [Path("."), *Path("post").iterdir()]:
         if not post.is_dir():
             continue
@@ -17,56 +19,77 @@ def collect(is_force):
             if outfile.exists() and not is_force:
                 continue
 
-            pending_items.append((infile, outfile))
+            pending_items.put((infile, outfile))
+            total.value += 1
 
-    return pending_items
 
-def generate(pending_items):
-    total = len(pending_items)
-    done = 0
+def generator(pending_items, lock, done, total, tmpl):
+    while True:
+        infile, outfile = pending_items.get()
+
+        body = None
+        with open(infile) as f:
+            body = (
+                sp.run(
+                    ["pandoc", "--mathjax", "-f", "markdown-smart"],
+                    stdin=f,
+                    capture_output=True,
+                )
+                .stdout.decode("utf-8")
+                .replace("\r", "")
+            )
+
+        with open(outfile, "w", encoding="utf-8") as f:
+            f.write(tmpl.format(body=body))
+
+        pending_items.task_done()
+
+        done.value += 1
+        print(f"{done.value}/{total.value}")
+
+
+def main():
+    P = argparse.ArgumentParser()
+    P.add_argument(
+        "--force", "-f", action="store_true", help="Force building all posts",
+    )
+    P.add_argument(
+        "--jobs", "-j", type=int, default=4, help="Number of jobs",
+    )
+
+    args = P.parse_args()
 
     tmpl = None
     with open(Path("scripts") / "template.html") as f:
         tmpl = f.read()
 
-    for infile, outfile in pending_items:
-        body = None
+    pending_items = JoinableQueue()
+    lock = Lock()
+    done, total = Value("i", 0), Value("i", 0)
 
-        with open(infile) as f:
-            body = sp.run(["pandoc", "--mathjax", "-f", "markdown-smart"],
-                stdin=f, capture_output=True).stdout.decode("utf-8").replace("\r", "")
-
-        with open(outfile, "w", encoding="utf-8") as f:
-            f.write(tmpl.format(body=body))
-
-        done += 1
-        print(f"{done}/{total}")
-
-def main():
-    P = argparse.ArgumentParser()
-    P.add_argument(
-        "--force",
-        "-f",
-        action='store_true',
-        help="Force building all posts",
+    coll = Process(
+        target=collector, args=(pending_items, lock, total, args.force)
     )
-    P.add_argument(
-        "--jobs",
-        "-j",
-        type=int,
-        default=4,
-        help="Number of jobs",
-    )
-
-    args = P.parse_args()
+    gens = [
+        Process(
+            target=generator, args=(pending_items, lock, done, total, tmpl)
+        )
+        for _ in range(args.jobs)
+    ]
 
     print("Collecting...")
-    pending_items = collect(args.force)
+    coll.start()
+    coll.join()
+    print(f"{done.value}/{total.value}")
 
     print("Generating...")
-    generate(pending_items)
+    for g in gens:
+        g.daemon = True
+        g.start()
 
+    pending_items.join()
     print("Job done!")
+
 
 if __name__ == "__main__":
     main()
